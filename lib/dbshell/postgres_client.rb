bb26c6d3a4f96a8e87cf14bb72ef283c86e8891a
Teddy Wing <git@teddywing.com>
2017-04-29 19:02:02 +0200

    PostgresClient: Basic implementation of #build_command
    Correctly builds the required arguments. Doesn't handle passwords at
    all.

--- lib/dbshell/postgres_client.rb
+++ lib/dbshell/postgres_client.rb
@@ -1,4 +1,18 @@
 class DBShell::PostgresClient
+  EXECUTABLE_NAME = 'psql'
+
   def self.build_command(db_info)
+    args = [EXECUTABLE_NAME]
+
+    host = db_info['host']
+    port = db_info['port']
+    db_name = db_info['database']
+    user = db_info['username']
+    password = db_info['password']
+
+    args.push('-U', user) if user
+    args.push('-h', host) if host
+    args.push('-p', port) if port
+    args.push(db_name)
   end
 end
