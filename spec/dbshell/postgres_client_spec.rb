6d35a516a7e4cda93f79455b2b9b60d13ddd80fa
Teddy Wing <git@teddywing.com>
2017-04-29 18:33:47 +0200

    postgres_client_spec.rb: Add missing `it` block
    Forgot to add an `it` block for my test, which resulted in it failing
    with a non-obvious error message.

--- spec/dbshell/postgres_client_spec.rb
+++ spec/dbshell/postgres_client_spec.rb
@@ -3,22 +3,24 @@ require 'dbshell/postgres_client'
 
 describe DBShell::PostgresClient do
   describe ".build_command" do
-    DBShell::PostgresClient.build_command({
-      'adapter' => 'postgresql',
-      'host' => 'mailmarehost',
-      'port' => 6027,
-      'username' => 'derpyhooves',
-      'password' => 'somepassword',
-      'database' => 'dbname'
-    }).must_equal([
-      'psql',
-      '-U',
-      'derpyhooves',
-      '-h',
-      'mailmarehost',
-      '-p',
-      '6027',
-      'dbname'
-    ])
+    it "builds basic arguments" do
+      DBShell::PostgresClient.build_command({
+        'adapter' => 'postgresql',
+        'host' => 'mailmarehost',
+        'port' => 6027,
+        'username' => 'derpyhooves',
+        'password' => 'somepassword',
+        'database' => 'dbname'
+      }).must_equal([
+        'psql',
+        '-U',
+        'derpyhooves',
+        '-h',
+        'mailmarehost',
+        '-p',
+        '6027',
+        'dbname'
+      ])
+    end
   end
 end
